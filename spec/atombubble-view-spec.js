4a667df3919cc82fa7e198c901b5d7a4a1fd0ccf
k1630477 <ming.x.wu@kcl.ac.uk>
2018-10-24 18:08:05 +0100

    Initial commit

---
+++ spec/atombubble-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtombubbleView from '../lib/atombubble-view';
+
+describe('AtombubbleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
